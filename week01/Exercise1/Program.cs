97461bc674c135cd63abd4bb7d851efd4840b7c8
ievbosaru <ievbosaru@byupathway.edu>
2025-07-05 13:35:29 -0700

    finished 2

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("what is your  first name? ");
+        string first = Console.ReadLine();
+       
+       Console.Write("what is your last name? ");
+        string last = Console.ReadLine();
+        
+        Console.Write($"your name is {last}, {first} {last}");
+       
+    
     }
 }
