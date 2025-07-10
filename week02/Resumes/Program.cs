53109f05fed017d747a71d78ee091670d176892a
ievbosaru <ievbosaru@byupathway.edu>
2025-07-10 09:33:30 -0700

    done

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,29 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Create Job 1
-        Job job1 = new Job();
-        job1._jobTitle = "Software Engineer";
-        job1._company = "Microsoft";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
-
-        // Create Job 2
-        Job job2 = new Job();
-        job2._jobTitle = "Manager";
-        job2._company = "Apple";
-        job2._startYear = 2022;
-        job2._endYear = 2023;
-
-        // Create Resume
-        Resume myResume = new Resume();
-        myResume._name = "Allison Rose";
-
-        // Add jobs to resume
-        myResume._jobs.Add(job1);
-        myResume._jobs.Add(job2);
-
-        // Display the full resume
-        myResume.Display();
+        Console.WriteLine("Hello World! This is the Resumes Project.");
     }
 }
