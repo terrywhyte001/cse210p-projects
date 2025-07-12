7df6195020e24bf1ed1386438a6783aa698f9012
ievbosaru <ievbosaru@byupathway.edu>
2025-07-12 09:35:33 -0700

    yes

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -7,9 +7,15 @@ public class PromptGenerator
     {
         "What was the best part of your day?",
         "What are you grateful for today?",
-        "Describe a challenge you faced today.",
+        "Describe a challenge you faced today?",
         "What made you smile today?",
-        "What did you learn today?"
+        "What did you learn today?",
+        "Who is someone you appreciate and why?", // <-- fixed quote and comma
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?"
     };
 
     public string GetRandomPrompt()
